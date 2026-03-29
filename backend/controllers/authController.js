18811de39c51624e7dc6a808889c47767ee60658
Elle <waraphon.koe@gmail.com>
2026-03-30 01:29:27 +1000

    feat: improve UX - my listings table, read-only listing detail, admin panel tables with dates, browse listings posted date

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -66,12 +66,12 @@ const updateUserProfile = async (req, res) => {
 };
 
 const getAllUsers = async (req, res) => {
-    try {
-        const users = await User.find({}).select('-password');
-        res.json(users);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+  try {
+    const users = await User.find({}).select('-password');
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = { registerUser, loginUser, updateUserProfile, getProfile, getAllUsers };
